b00078821bbda585e01119fac7c2adce22e6f313
tokukiyu <tokkiyuam@gmail.com>
2023-08-10 00:13:46 +0300

    updated

--- tri-fabonnacci.py
+++ tri-fabonnacci.py
@@ -1,10 +1,17 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-            if n == 0:
-             return 0
-            elif n == 1 or n == 2:
-             return 1
-            else:
-             return self.tribonacci(n-1)+self.tribonacci(n-2)+self.tribonacci(n-3)
+        if n == 0:
+            return 0
+        elif n == 1 or n == 2:
+            return 1
+        
+        dp = [0] * (n + 1)
+        dp[1] = dp[2] = 1
+        
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+        
+        return dp[n]
+
 sol=Solution()
-print(sol.tribonacci(4))
+print(sol.tribonacci(33))
