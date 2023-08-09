cb161d7b3e3cbee3eec34de2295b2fb1852fd341
tokukiyu <tokkiyuam@gmail.com>
2023-08-09 15:30:15 +0300

    Fibonacci

---
+++ fabonnici.py
@@ -0,0 +1,10 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if(n<=1):
+            return n
+        
+        else:
+            return self.fib(n-1)+self.fib(n-2)
+
+sol=Solution()
+print(sol.fib(4))
