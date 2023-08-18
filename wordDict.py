a6b4d122b9dc1da2cda5b8f9ee40f926a9fd28b2
tokukiyu <tokkiyuam@gmail.com>
2023-08-18 08:53:36 +0300

    code

---
+++ wordDict.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n = len(s)
+        # Create a set for faster word lookup
+        word_set = set(wordDict)
+        
+        # dp[i] will be True if s[:i] can be segmented into words from wordDict
+        dp = [False] * (n + 1)
+        dp[0] = True  # Empty string is always in the wordDict
+        
+        for i in range(1, n + 1):
+            for j in range(i):
+                # Check if the current substring can be segmented
+                if dp[j] and s[j:i] in word_set:
+                    dp[i] = True
+                    break
+        
+        return dp[n]
