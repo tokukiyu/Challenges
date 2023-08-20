b3c8dd132eb732bdbf8b345a63c658b3afb6ffff
tokukiyu <tokkiyuam@gmail.com>
2023-08-20 09:29:44 +0300

    a

--- edit-distance.py
+++ edit-distance.py
@@ -12,7 +12,7 @@ class Solution:
         for j in range(n + 1):
             dp[0][j] = j
             
-        # Fill in the DP arra y
+        # Fill in the DP array
         for i in range(1, m + 1):
             for j in range(1, n + 1):
                 if word1[i - 1] == word2[j - 1]:
