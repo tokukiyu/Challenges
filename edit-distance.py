8cd32a7a82c790483dfdb4da306831a34794bde7
tokukiyu <tokkiyuam@gmail.com>
2023-08-20 09:29:54 +0300

    a

--- edit-distance.py
+++ edit-distance.py
@@ -11,7 +11,7 @@ class Solution:
             dp[i][0] = i
         for j in range(n + 1):
             dp[0][j] = j
-            
+             
         # Fill in the DP array
         for i in range(1, m + 1):
             for j in range(1, n + 1):
