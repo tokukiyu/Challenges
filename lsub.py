32eeff1739211b4a8d85b2e14b48ec7e66bad7f6
tokukiyu <tokkiyuam@gmail.com>
2023-08-25 08:42:07 +0300

    update

--- lsub.py
+++ lsub.py
@@ -1,14 +1,16 @@
 from typing import List
+
+
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         if not nums:
             return 0
-               # Initialize an array to store the length of longest increasing subsequences
+            # Initialize an array to store the length of longest increasing subsequences
         dp = [1] * len(nums)
-        
+
         for i in range(len(nums)):
             for j in range(i):
                 if nums[i] > nums[j]:
                     dp[i] = max(dp[i], dp[j] + 1)
-        
+
         return max(dp)
