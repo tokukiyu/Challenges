c6d9cbf6c1f46286ebdb5ae54759d476ecb85eb1
tokukiyu <tokkiyuam@gmail.com>
2023-08-26 08:51:29 +0300

    upt

--- findNumbinlist.py
+++ findNumbinlist.py
@@ -13,7 +13,6 @@ class Solution:
                         lengths[i] = lengths[j] + 1
                         counts[i] = counts[j]
         
-        
         max_length = max(lengths)
         result = 0
         for i in range(n):
