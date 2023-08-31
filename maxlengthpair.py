1f7845e37c3959370bd5da21ac0ea9d5c2386b88
tokukiyu <tokkiyuam@gmail.com>
2023-08-31 09:15:17 +0300

    maximum length pair

---
+++ maxlengthpair.py
@@ -0,0 +1,13 @@
+from typing import List
+class Solution:
+    def findLongestChain(self, pairs: List [List[int]]) -> int:
+        pairs.sort(key=lambda x: x[1])  # Sort pairs based on the second element
+        
+        dp = [1] * len(pairs)  # Initialize a dynamic programming array
+        
+        for i in range(1, len(pairs)):
+            for j in range(i):
+                if pairs[j][1] < pairs[i][0]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        
+        return max(dp)  # Return the maximum value in the dp array
